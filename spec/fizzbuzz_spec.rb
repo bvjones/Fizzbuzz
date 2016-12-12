1f5a15ed93aaddc360513058d24ed2bf6f8029a8
Ben Vaughan-Jones <benvaughanjones@gmail.com>
2016-12-12 18:15:54 +0000

    Testing for multiples of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,13 @@ describe 'fizzbuzz' do
 	it 'returns "fizz" when passed 3' do
 		expect(fizzbuzz(3)).to eq 'fizz'
 	end
+	it 'returns "fizz" when a passed a multiple of 3' do
+		expect(fizzbuzz(6)).to eq 'fizz'
+		expect(fizzbuzz(9)).to eq 'fizz'
+		expect(fizzbuzz(12)).to eq 'fizz'
+	end
+	it "returns the message 'Buzz' when passed a number that is a multiple of 5." do
+		expect(fizzbuzz(5)).to eq 'Buzz'
+	end
+
 end
